645918b9dbfe655059272fd0edfd3a3826258982
Nathan <68994507+nathan-uch@users.noreply.github.com>
2022-01-24 21:59:57 -0800

    added methods that will be used in the game, still need to organize better

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ This project is the creation of the game tic tac toe.
 The goal is to use information learned on array methods, object creation, modules and factories. I am also to make my code least globally polluted as possible by making variables only useable inside functions or through returns. 
 
 I will also use this project to try and write my code in more organized sections (where possible) by created comments and descriptions for other people who analyze the code.
+
+Challenges being faced: 
+-How should I organize my separate modules and factory functions?
+-Which methods and variables should reside in what?
+-How can one module use another's variable?
--- script.js
+++ script.js
@@ -1,39 +1,95 @@
+//Player Factory 
+//this factory will objects for each player with mark of X or O
+//this will also be used for the computer player
+const CreatePlayer = (name, mark) => {
+    this.name = name;
+    this.mark = mark;
+    
+    return {
+        name,
+        mark
+    };
+};
 
-// Board Object
-// let gameBoard = (function() {
-//     'use strict';
+//Game Board Module 
+//this module create the board array and render the board
+const GameBoard = (function() {
+    'use strict';
 
-    let board = ["X", "", "O", "X", "X", "", "O", "O", ""];
-    let cell = document.querySelectorAll(".cell");
+    let array = ["X", "", "", "O", "", "", "", "", ""];
 
-    function renderBoard() {
-        for (let c = 0; c < cell.length; c++) { //loop through each cell
-            for (let i = 0; i < board.length; i++) { //loop through board array
-                console.log(board[i]);
-                if (board[i] == "X" && board.indexOf(board[i]) == cell[c].id) {
-                    cell[c].textContent = "X";
-                } else if (board[i] == "O" && board.indexOf(board[i]) == cell[c].id) {
-                    cell[c].textContent = "O";
+    function arrayBoard() {
+        return array;
+    }
+
+    const renderContents = () => {
+        let cell = document.querySelectorAll(".cell");
+        for (let c = 0; c < cell.length; c++) { 
+            for (let i = 0; i < arrayBoard.length; i++) { 
+                if (cell[c].id == i) { 
+                    cell[c].textContent = arrayBoard[i];
                 };
             };
         };
     };
 
-//     return {
-//         board: board,
-//         renderBoard
-//     };
-// })();
+    return { 
+        arrayBoard: arrayBoard,
+        renderContents
+    };
+})();
 
-renderBoard();
+//Game Controller Module
+//this module will check different variables each turn
+const GameControl = (function() {
+    'use strict';
 
-var Cars = ["Nano", "Volvo", "BMW", "Nano", "VW", "Nano"];
-function getAllIndexes(arr, val) {
-    var indexes = [], i = -1;
-    while ((i = arr.indexOf(val, i+1)) != -1){
-        indexes.push(i);
+    //Event Listener for clicking on the board
+    let cell = document.querySelectorAll(".cell");
+    let index;
+    let xCount;
+    let oCount;
+
+    const checkTurn = () => {
+        for (let c = 0; c > GameBoard.arrayBoard.length; c++) {
+            if (GameBoard.arrayBoardray[c] == "X") {
+                xCount++;
+            } else if (GameBoard.arrayBoardray[c] == "O") {
+                oCount++;
+            };
+        };
+    };
+
+    const checkExistingMarks = () => {
+    };
+
+    const addMarkToArray = (index, xCount, oCount) => {
+        if (xCount == 0 || xCount == oCount ) {
+            GameBoard.arrayBoard[index] = 'X';
+        } else if (oCount < xCount) {
+            GameBoard.arrayBoard[index] = 'O';
+        };
+    };
+
+    const checkWinner = () => {
+    };
+
+    cell.forEach((button) => {
+        button.addEventListener('click', () => {
+            index = button.id;
+            checkTurn;
+            //checkExistingMarks;
+            addMarkToArray(index);
+            GameBoard.renderContents();
+            //this.checkWinner;
+        });
+    });
+
+    return {
     }
-    return indexes;
-}
 
-var indexes = getAllIndexes(Cars, "Nano");
+})();
+
+
+const player1 = CreatePlayer('bob', 'X');
+const player2 = CreatePlayer('mary', 'O');
